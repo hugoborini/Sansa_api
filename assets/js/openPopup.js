a966790687f5d32dd97ea2fcb460cd2134403812
Hgo0123 <hcordillot@gmail.com>
2022-07-08 13:55:47 +0200

    fix popup

--- assets/js/openPopup.js
+++ assets/js/openPopup.js
@@ -4,6 +4,7 @@ class OpenPopup {
     constructor(triggerElem) {
         this.expandList = triggerElem.querySelector('.popup');
         this.svg = triggerElem.querySelector('svg');
+        this.dots = document.querySelectorAll('.open-popup');
         this.toggle(triggerElem);
     }
 
@@ -12,8 +13,19 @@ class OpenPopup {
             this.expandList.classList.toggle('d-n');
             this.svg.classList.toggle('bgc-blue-light');
             this.svg.classList.toggle('br-xl');
+            this.closeAll(triggerElem);
         });
     };
+
+    closeAll(triggerElem) {
+        this.dots.forEach((dot) => {
+            if(triggerElem !== dot ) {
+                dot.querySelector('.popup').classList.add('d-n');
+                dot.querySelector('svg').classList.remove('bgc-blue-light');
+                dot.querySelector('svg').classList.remove('br-xl');
+            }
+        })
+    }
 }
 
 document.querySelectorAll('.open-popup').forEach(el => {
