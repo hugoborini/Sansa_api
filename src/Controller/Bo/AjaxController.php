6d23b10b8694c488e1fa1a4386de5f7b687e6341
hugoborini <hugo.borini@hetic.net>
2022-07-09 17:14:55 +0200

    kfd

--- src/Controller/Bo/AjaxController.php
+++ src/Controller/Bo/AjaxController.php
@@ -4,17 +4,18 @@ namespace App\Controller\Bo;
 
 use App\Entity\Organization;
 use Geocoder\Query\GeocodeQuery;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AjaxController extends AbstractController
 {
     /**
      * @Route("/bo/ajax/addOrga", name="app_bo_ajax_add_orga", methods="POST")
      */
-    public function addOrgAjax(Request $request): JsonResponse
+    public function addOrgAjax(Request $request, ObjectManager $manager): JsonResponse
     {
         $httpClient = new \Http\Adapter\Guzzle6\Client();
         $provider = new \Geocoder\Provider\GoogleMaps\GoogleMaps($httpClient, null, 'AIzaSyBiG9V9KBLLv-TYeu8gcuc-yWmEG6jqVn8');
@@ -24,18 +25,48 @@ class AjaxController extends AbstractController
 
         $organisationObj = new Organization();
 
-        // $organisationObj->setOrganizationOwner();
-        // $organisationObj->setOrganizationName();
-        // $organisationObj->setAdress();
-        // $organisationObj->setDescription();
-        // $organisationObj->setLastUpdata(new DateTime(str_replace("/", "-", trim($date[1], " "))));
-        // $organisationObj->setPhoneNumber();
-        // $organisationObj->setWebsite();
-        // $organisationObj->setSpokenLanguage();
-        // $organisationObj->setImportanteInformation();
-        // $result = $geocoder->geocodeQuery(GeocodeQuery::create($org->address));
-        // $organisationObj->setLongitude($result->all()[0]->getCoordinates()->getLongitude());
-        // $organisationObj->setLatitude($result->all()[0]->getCoordinates()->getLatitude());
+        $data = $request->request->all()["data"];
+
+
+        $organisationObj->setOrganizationOwner($this->getUser()->getId());
+        $organisationObj->setOrganizationName($data["association_name"]);
+        $organisationObj->setAdress($data["address"]);
+        $organisationObj->setDescription($data["mission"]);
+        $organisationObj->setLastUpdata(new DateTime(date("Ymd")));
+        $organisationObj->setPhoneNumber($data["telephone"]);
+        $organisationObj->setWebsite($data["site"]);
+        $organisationObj->setSpokenLanguage(trim(implode(", ", $data["langages"]), ", "));
+        $organisationObj->setImportanteInformation("Warning");
+        $result = $geocoder->geocodeQuery(GeocodeQuery::create($data["address"]));
+        $organisationObj->setLongitude($result->all()[0]->getCoordinates()->getLongitude());
+        $organisationObj->setLatitude($result->all()[0]->getCoordinates()->getLatitude());
+        $organisationObj->setSiret($data["siret"]);
+        $organisationObj->setRna($data["rna_number"]);
+        $organisationObj->setNote(rand(0, 5));
+        $organisationObj->setValitated(false);
+        $organisationObj->setByAppointment($data["appointement"]);
+        $manager->persist($organisationObj);
+
+        $preferencialObj = new PreferencialWelcome();
+        $preferencialObj->setValue($data["accueil_type"]);
+        $preferencialObj->setOrganisationId($organisationObj);
+        $manager->persist($preferencialObj);
+
+        $hoursObj = new Hours();
+        $hoursObj->setOrganizationId($organisationObj);
+        $hoursObj->setMonday($data["schedules"]["Lundi"]);
+        $hoursObj->setTuesday($data["schedules"]["Mardi"]);
+        $hoursObj->setWednesday($data["schedules"]["Mercredi"]);
+        $hoursObj->setThurday($data["schedules"]["Jeudi"]);
+        $hoursObj->setFriday($data["schedules"]["Vendredi"]);
+        $hoursObj->setSaturday($data["schedules"]["Samedi"]);
+        $hoursObj->setSunday($data["schedules"]["Dimanche"]);
+        $manager->persist($hoursObj);
+
+
+        $manager->flush();
+
+
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
