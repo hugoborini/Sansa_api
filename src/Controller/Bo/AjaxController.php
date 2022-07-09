8260dbd0bcee01e969102266591210d411d8ba81
hugoborini <hugo.borini@hetic.net>
2022-07-09 18:34:17 +0200

    finish ajax

--- src/Controller/Bo/AjaxController.php
+++ src/Controller/Bo/AjaxController.php
@@ -2,25 +2,30 @@
 
 namespace App\Controller\Bo;
 
+use App\Entity\OrganizationOwner;
+use DateTime;
+use App\Entity\Hours;
 use App\Entity\Organization;
 use Geocoder\Query\GeocodeQuery;
+use App\Entity\PreferencialWelcome;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\OrganizationRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\OrganizationOwnerRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use DateTime;
-use App\Entity\PreferencialWelcome;
-use App\Entity\Hours;
 
 class AjaxController extends AbstractController
 {
     /**
-     * @Route("/bo/ajax/addOrga", name="app_bo_ajax_add_orga", methods="POST")
+     * @Route("/bo/ajax/addOrga", name="app_bo_ajax_add_orga", methods="get")
      */
-    public function addOrgAjax(Request $request, EntityManagerInterface $manager, OrganizationRepository $orgaRepo): JsonResponse
+    public function addOrgAjax(Request $request, EntityManagerInterface $manager, OrganizationOwnerRepository $orgaOwnerRepo): JsonResponse
     {
+
+
         $httpClient = new \Http\Adapter\Guzzle6\Client();
         $provider = new \Geocoder\Provider\GoogleMaps\GoogleMaps($httpClient, null, 'AIzaSyBiG9V9KBLLv-TYeu8gcuc-yWmEG6jqVn8');
         $geocoder = new \Geocoder\StatefulGeocoder($provider, 'en');
@@ -28,18 +33,20 @@ class AjaxController extends AbstractController
 
 
         $organisationObj = new Organization();
-
+        
         $data = $request->request->all()["data"];
+        $userObj = $orgaOwnerRepo->findById($this->getUser()->getId());
+        $orgaOwnerObj = $orgaOwnerRepo->find($this->getUser()->getId());
 
 
 
-        $organisationObj->setOrganizationOwner($orgaRepo->find($this->getUser()->getId()));
+        $organisationObj->setOrganizationOwner($userObj);
         $organisationObj->setOrganizationName($data["association_name"]);
         $organisationObj->setAdress($data["address"]);
         $organisationObj->setDescription($data["mission"]);
         $organisationObj->setLastUpdata(new DateTime(date("Ymd")));
         $organisationObj->setPhoneNumber($data["telephone"]);
-        $organisationObj->setWebsite($data["site"]);
+        $organisationObj->setWebsite($data["siteName"]);
         $organisationObj->setSpokenLanguage(trim(implode(", ", $data["langages"]), ", "));
         $organisationObj->setImportanteInformation("Warning");
         $result = $geocoder->geocodeQuery(GeocodeQuery::create($data["address"]));
@@ -68,14 +75,12 @@ class AjaxController extends AbstractController
         $hoursObj->setSunday($data["schedules"]["Dimanche"]);
         $manager->persist($hoursObj);
 
+        $orgaOwnerObj->setHasasso(True);
 
         $manager->flush();
 
 
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AjaxController.php',
-        ]);
+        return $this->json(["orga add to bdd"]);
     }
 }
