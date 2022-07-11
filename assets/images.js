43319d1ccfb2075d56834e322b10ef87066fd0a2
Hgo0123 <hcordillot@gmail.com>
2022-07-11 17:45:19 +0200

    add asso dashboard, update form, fix onboarding

--- assets/images.js
+++ assets/images.js
@@ -2,4 +2,6 @@ import "./images/designer.png";
 import "./images/note_taking.png";
 import "./images/final_step.png";
 import "./images/spotlight.png";
+import "./images/edit_illu_identity.png";
+import "./images/edit_illu_mission.png";
 
