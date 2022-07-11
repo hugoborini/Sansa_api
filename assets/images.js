2f8a340a2cb8c3333c940a927bd6d4a8746d2252
nawel berrichi <nawel.berrichi@hetic.net>
2022-07-11 18:37:52 +0200

    site vitrine

--- assets/images.js
+++ assets/images.js
@@ -2,4 +2,16 @@ import "./images/designer.png";
 import "./images/note_taking.png";
 import "./images/final_step.png";
 import "./images/spotlight.png";
-
+import "./images/account-validated.png";
+import "./images/aurore.png";
+import "./images/create-account.png";
+import "./images/hero-page.png";
+import "./images/image_50.png";
+import "./images/mairie_paris.png";
+import "./images/pfdp.png";
+import "./images/resto-du-coeur.png";
+import "./images/tablet-iphone.svg";
+import "./images/tablet-location.png";
+import "./images/validation-account.png";
+import "./images/world-connection.svg";
+import "./images/Map.png";
